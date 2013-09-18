2e027618d21fd8e4ee945e6539a7d74cd179e5ba
clux <analsandblaster@gmail.com>
2013-09-18 22:16:04 +0100

    use gu 0.1 and irc-stream in combination

--- irc.js
+++ irc.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
 var cfgPath = require('confortable')('.clvr.json', process.cwd());
-
 if (!cfgPath) {
   throw new Error("When loading wolfram-irc externally, a local config is required");
 }
-console.log('using: ' + cfgPath);
 var cfg = require(cfgPath);
 
-require('gu')(cfg.server, cfg.name, {
+var join = require('path').join;
+var gu = require('gu')(join(__dirname, 'bot'), ['clvr.js']);
+var ircStream = require('irc-stream')(cfg.server, cfg.name, {
   userName: 'IAmA',
   realName: 'clever',
   debug: false,
   channels: [cfg.chan],
-}, require('path').join(__dirname, 'bot'), ['clvr.js']);
+});
+
+ircStream.pipe(gu).pipe(ircStream);
