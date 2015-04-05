aac9ce7c3599061bfc68d1e40332a3030627b283
clux <analsandblaster@gmail.com>
2015-04-05 14:18:27 +0100

    use new version of stream specifications - gu@0.5.0 and irc-stream@0.3.0

--- test/response.test.js
+++ test/response.test.js
@@ -5,12 +5,13 @@ var Gu = require('gu')
 exports.response = function (t) {
   var clvr = new Gu(guParams.scriptdir, guParams.files, { noReload: true });
   sulfur.absorb(clvr.log, 'gu');
-  clvr.write({user: '#chan:clux', name: 'clux', message: 'hi clvr'});
+  clvr.write({user: 'clux', channel: '#test', message: 'hi clvr'});
 
   clvr.on('readable', function () {
     var resp = clvr.read();
     t.ok(resp, 'got a response');
-    t.equal(resp.user, '#chan:clux', 'addressed to me');
+    t.equal(resp.user, 'clux', 'addressed to me');
+    t.equal(resp.channel, '#test', 'in requested channel');
     // TODO: if it starts failing - create a test to ensure
     // resp is not equal to "<html>"" or whatever it returns when permissions fail
     t.done();
